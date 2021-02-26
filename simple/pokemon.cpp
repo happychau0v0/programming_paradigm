6574181dc986ceff1cd373bac42e84587cde4b89
happychau0v0 <happychau0v0@gmail.com>
2021-02-26 10:18:34 +0800

    updated many stuffs

--- simple/pokemon.cpp
+++ simple/pokemon.cpp
@@ -1,18 +1,14 @@
 #include "pokemon.h"
 
+#include <cstdio>
+#include <cstring>
+
 Pokemon Create(Type t, const char* name) {
     Pokemon p;
     p.t = t;
     strcpy(p.name, name);
     p.hp = 20;
     p.atk = 1;
-
-    if (t == SQUIRTLE) {
-        p.atk = 3;
-    } else if (t == BULBASAUR) {
-        p.hp = 30, p.atk = 2;
-    }
-
     return p;
 }
 
@@ -30,18 +26,6 @@ void TakeDamge(Pokemon* p, int dmg) {
 
 void Attack(Pokemon* p, Pokemon* other) {
     printf("%s attacked %s!\n", p->name, other->name);
-    if (p->t == CHARMANDER) {
-        TakeDamge(other, 3);
-        TakeDamge(other, 2);
-        TakeDamge(other, 1);
-    } else {
-        TakeDamge(other, p->atk);
-    }
-    printf("\n");
-}
-
-void Thunderbolt(Pokemon* p, Pokemon* other) {
-    printf("%s used 10,000,000 volt thunderbolt!\n", p->name);
-    TakeDamge(other, 10);
+    TakeDamge(other, p->atk);
     printf("\n");
 }
